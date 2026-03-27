ca12c5813695a5460acb643aa61e256612a879ba
TrueAYBYouTube <ayuubajmal10@outlook.com>
2026-03-27 19:43:49 +0000

    Implementing a Datatable of Employee Information

---
+++ DATATABLE THAT CONTAINS EMPLOYEE INFORMATION.py
@@ -0,0 +1,12 @@
+df = pd.DataFrame(
+    {
+        "Name": [
+            "Johnathan",
+            "Joe"
+            "Nathan"
+        ],
+        "Age": [22, 35, 58],
+        "Sex": ["male", "male", "female"],
+        "Role": ["Manger", "Finance Manger", "IT support","UI and UX Designer"]
+    }
+)
